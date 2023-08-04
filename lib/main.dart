c06b9fc8761d881a142a34449de45d3eaabaaab4
Milena_Santos <mcs9823@gmail.com>
2023-08-04 19:49:29 +0000

    adicionando estilos de fontes diferentes e icones (para resolver o desafio anterior era só adicionar um container com uma row ou column)

--- lib/main.dart
+++ lib/main.dart
@@ -11,40 +11,48 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Align(
-                alignment: AlignmentDirectional.topStart,
-                child: Container(
-                  width: 100.0,
-                  color: Colors.amber,
-                  child: Text('Container 1'),
-                ),
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.amber,
+              radius: 50.0,
+              backgroundImage: NetworkImage(
+                  'https://images.pexels.com/photos/13733669/pexels-photo-13733669.jpeg?auto=compress&cs=tinysrgb&w=600'),
+            ),
+            Text(
+              'Milena',
+              style: TextStyle(
+                  fontFamily: 'KaushanScript',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Web Developer',
+              style: TextStyle(
+                  fontFamily: 'SourceCodePro',
+                  color: Colors.teal[100],
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    '+55 11 96692 6572',
+                    style: TextStyle(
+                        color: Colors.teal[900], fontFamily: 'SourceCodePro'),
+                  )
+                ],
               ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.deepPurple,
-                child: Text('Container 2'),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.deepOrangeAccent,
-                child: Text('Container 3'),
-              ),
-              Align(
-                alignment: Alignment(1, 1),
-                child: Container(
-                  width: 100.0,
-                  color: Colors.deepOrange,
-                  child: Text('Container 4'),
-                ),
-              )
-            ],
-          ), // o container só pode ter um child
-        ),
+            )
+          ],
+        )),
       ),
     );
   }
